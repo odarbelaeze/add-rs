e67db377c0d91d3f501f0d22eb78fd7653fb1a0b
Oscar David Arbeláez <oscar.arbelaez@epfl.ch>
2019-04-25 16:56:02 +0200

    Code the static add function

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use docopt::Docopt;
+use serde::Deserialize;
+
+const USAGE: &str = "
+Add.
+
+Usage:
+    add <x> <y>
+    add (-h | --help)
+
+Options:
+  -h --help     Show this screen.
+";
+
+#[derive(Debug, Deserialize)]
+struct Args {
+    arg_x: f64,
+    arg_y: f64,
+}
+
+fn main() {
+    let args: Args = Docopt::new(USAGE)
+        .and_then(|d| d.deserialize())
+        .unwrap_or_else(|e| e.exit());
+    println!("{}", args.arg_x + args.arg_y);
+}
