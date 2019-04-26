c16ec614c636b13cf0492ecb8b2288a448302f64
Oscar David Arbeláez <oscar.arbelaez@epfl.ch>
2019-04-26 14:51:01 +0200

    Make it compatible with the tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,14 @@
 use docopt::Docopt;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
+use serde_json::{from_reader, to_writer_pretty};
+use std::error::Error;
+use std::fs::File;
 
 const USAGE: &str = "
 Add.
 
 Usage:
-    add <x> <y>
+    add <input> <output>
     add (-h | --help)
 
 Options:
@@ -14,13 +17,33 @@ Options:
 
 #[derive(Debug, Deserialize)]
 struct Args {
-    arg_x: f64,
-    arg_y: f64,
+    arg_input: String,
+    arg_output: String,
 }
 
-fn main() {
+#[derive(Debug, Deserialize)]
+struct InputFormat {
+    x: f64,
+    y: f64,
+}
+
+#[derive(Debug, Serialize)]
+struct OutputFormat {
+    sum: f64,
+}
+
+fn main() -> Result<(), Box<Error>> {
     let args: Args = Docopt::new(USAGE)
         .and_then(|d| d.deserialize())
         .unwrap_or_else(|e| e.exit());
-    println!("{}", args.arg_x + args.arg_y);
+    let input = File::open(args.arg_input)?;
+    let input: InputFormat = from_reader(input)?;
+    let output = File::create(args.arg_output)?;
+    to_writer_pretty(
+        output,
+        &OutputFormat {
+            sum: input.x + input.y,
+        },
+    )?;
+    Ok(())
 }
